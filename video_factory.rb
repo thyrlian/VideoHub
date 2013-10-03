792b637446484de47302e7f4dc12ea1ac5be239f
Jing Li <thyrlian@gmail.com>
2013-10-03 22:36:57 +0200

    Added VideoFactory to list all videos under given directory

---
+++ video_factory.rb
@@ -0,0 +1,19 @@
+module VideoHub
+  class VideoFactory
+    class << self
+      def get_all_videos(dir, *supported_formats)
+        videos = Dir["#{dir}/**/*"].select { |f| File.file?(f) }.sort
+        unless supported_formats.empty?
+          supported_formats.map! { |sf| sf.to_s.downcase }
+          videos.select! do |video|
+            video_ext = File.extname(video).gsub(/^\./, '').downcase
+            supported_formats.include?(video_ext)
+          end
+        end
+        videos
+      end
+    end
+
+    private_class_method :new
+  end
+end
